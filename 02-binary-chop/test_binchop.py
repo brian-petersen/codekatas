8366a655ab67331df428a428c8d18d6f2887475b
Brian Petersen <skiwalk3r@gmail.com>
2019-11-16 17:32:25 -0700

    Refactor how binchop tests run
    Makes it so testng new binchop implementations is easier by using
    the provided test suite.

--- 02-binary-chop/test_binchop.py
+++ 02-binary-chop/test_binchop.py
@@ -5,27 +5,31 @@ from binchop import chop_iterative
 
 class TestBinChop(unittest.TestCase):
     def test_chop_iterative(self):
-        self.assertEqual(-1, chop_iterative(3, []))
-        self.assertEqual(-1, chop_iterative(3, [1]))
-        self.assertEqual(0,  chop_iterative(1, [1]))
+        run_tests(self, chop_iterative)
 
-        self.assertEqual(0,  chop_iterative(1, [1, 3, 5]))
-        self.assertEqual(1,  chop_iterative(3, [1, 3, 5]))
-        self.assertEqual(2,  chop_iterative(5, [1, 3, 5]))
-        self.assertEqual(-1, chop_iterative(0, [1, 3, 5]))
-        self.assertEqual(-1, chop_iterative(2, [1, 3, 5]))
-        self.assertEqual(-1, chop_iterative(4, [1, 3, 5]))
-        self.assertEqual(-1, chop_iterative(6, [1, 3, 5]))
 
-        self.assertEqual(0,  chop_iterative(1, [1, 3, 5, 7]))
-        self.assertEqual(1,  chop_iterative(3, [1, 3, 5, 7]))
-        self.assertEqual(2,  chop_iterative(5, [1, 3, 5, 7]))
-        self.assertEqual(3,  chop_iterative(7, [1, 3, 5, 7]))
-        self.assertEqual(-1, chop_iterative(0, [1, 3, 5, 7]))
-        self.assertEqual(-1, chop_iterative(2, [1, 3, 5, 7]))
-        self.assertEqual(-1, chop_iterative(4, [1, 3, 5, 7]))
-        self.assertEqual(-1, chop_iterative(6, [1, 3, 5, 7]))
-        self.assertEqual(-1, chop_iterative(8, [1, 3, 5, 7]))
+def run_tests(test_case, chop_impl):
+    test_case.assertEqual(-1, chop_impl(3, []))
+    test_case.assertEqual(-1, chop_impl(3, [1]))
+    test_case.assertEqual(0,  chop_impl(1, [1]))
+
+    test_case.assertEqual(0,  chop_impl(1, [1, 3, 5]))
+    test_case.assertEqual(1,  chop_impl(3, [1, 3, 5]))
+    test_case.assertEqual(2,  chop_impl(5, [1, 3, 5]))
+    test_case.assertEqual(-1, chop_impl(0, [1, 3, 5]))
+    test_case.assertEqual(-1, chop_impl(2, [1, 3, 5]))
+    test_case.assertEqual(-1, chop_impl(4, [1, 3, 5]))
+    test_case.assertEqual(-1, chop_impl(6, [1, 3, 5]))
+
+    test_case.assertEqual(0,  chop_impl(1, [1, 3, 5, 7]))
+    test_case.assertEqual(1,  chop_impl(3, [1, 3, 5, 7]))
+    test_case.assertEqual(2,  chop_impl(5, [1, 3, 5, 7]))
+    test_case.assertEqual(3,  chop_impl(7, [1, 3, 5, 7]))
+    test_case.assertEqual(-1, chop_impl(0, [1, 3, 5, 7]))
+    test_case.assertEqual(-1, chop_impl(2, [1, 3, 5, 7]))
+    test_case.assertEqual(-1, chop_impl(4, [1, 3, 5, 7]))
+    test_case.assertEqual(-1, chop_impl(6, [1, 3, 5, 7]))
+    test_case.assertEqual(-1, chop_impl(8, [1, 3, 5, 7]))
 
 
 if __name__ == '__main__':
